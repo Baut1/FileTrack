a64cb59369b02227a11f75908d5d2e8387cebd55
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-07-24 23:13:17 -0300

    app structure  refactors

--- actions.py
+++ actions.py
@@ -1,4 +1,7 @@
+# imports
 import os
+
+# tkinter
 from tkinter import *
 
 from google.auth.transport.requests import Request
@@ -16,16 +19,26 @@ SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = os.environ.get('SPREADSHEET_ID')
-SAMPLE_RANGE_NAME = "Turnos!A1:G10"
+SAMPLE_RANGE_NAME = os.environ.get('RANGE_NAME')
 
-creds = None
+# intitialize data values
+values = [[]]
 
-if os.path.exists("token.json"):
+# connection to the API
+def main():
+  """Shows basic usage of the Sheets API.
+  Prints values from a sample spreadsheet.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
+      creds.refresh(Request())
     else:
       flow = InstalledAppFlow.from_client_secrets_file(
           "credentials.json", SCOPES
@@ -34,9 +47,125 @@ if not creds or not creds.valid:
     # Save the credentials for the next run
     with open("token.json", "w") as token:
       token.write(creds.to_json())
+      
+  try:
+    service = build("sheets", "v4", credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = (
+        sheet.values()
+        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+        .execute()
+    )
+    global values
+    values = result.get("values", [])
+
+    if not values:
+      print("No data found.")
+      return
+    
+  except HttpError as err:
+    print(err)
+
+
+    # actions
+    # print all
+    def print_all(values):
+        for row in values:
+            print(row)
+
+    # print row given its id
+    def print_by_id(values, id):
+        for row in values:
+            if row[0] == id:
+                print(row)
+
+    # print all rows that match the client name
+    def print_filtered_by_client(values, client_name):
+        result = filter(lambda row: client_name.lower() in row[1].lower(), values)
+        print(list(result))
+
+    # print all rows that match the model name
+    def print_filtered_by_model(values, model_name):
+        result = filter(lambda row: model_name.lower() in row[3].lower(), values)
+        print(list(result))
+
+    # print all rows that match the date
+    def print_filtered_by_date(values, date):
+        result = filter(lambda row: date in row[5], values)
+        print(list(result))
+
+    # print most recent 10 rows
+    def print_filtered_by_date_last_ten(values):
+        values.sort(key = lambda row: row[5], reverse=True)
+        print(list(values[0:10]))
+
+    # find the row index ("range") given an id
+    def find_row_index(sheet_values, column_index, target_value):
+        for i, row in enumerate(sheet_values):
+            if len(row) > column_index and row[column_index] == str(target_value):
+                return i + 1  # Sheets API uses 1-based indices
+        return None
+
+    # update row chosen by its id
+    def update_by_id(sheet_values, target_id):
+        column_name = 'id'
+        new_value = 'New Value'
+        # Find the row index based on the 'id' column
+        column_index = sheet_values[0].index(column_name) if sheet_values and column_name in sheet_values[0] else None
+        row_index = find_row_index(sheet_values, column_index, target_id)
+
+        if row_index is not None:
+            try:
+                service = build("sheets", "v4", credentials=creds)
+                # Call the Sheets API
+                sheet = service.spreadsheets()
+                result = (
+                    sheet.values()
+                    .update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range=f'Turnos!G{row_index}',
+                            valueInputOption='RAW',
+                            body={'values': [[new_value]]})
+                )
+                # Execute the request
+                response = result.execute()
+                # Print the updated range and values
+                print(f"Updated range: {response['updatedRange']}")
+                
+            except HttpError as err:
+                print(err)
+        else:
+            print(f"Row with '{column_name}' = '{target_id}' not found.")
+
+    # add new row to the table
+    def add(sheet_values):
+
+        index_to_extract = 0
+        ids_array = [int(sub_array[index_to_extract]) for sub_array in sheet_values] # get array of ids
+        new_row_values = [max(ids_array) + 1, 'Ana', 'agonzalez@gmail.com', 'Samsung S23', 'Boton roto', '2024-02-01', 'asd']
+        try:
+                service = build("sheets", "v4", credentials=creds)
+
+                # Call the Sheets API
+                sheet = service.spreadsheets()
+                append_request = (
+                    sheet.values()
+                    .append(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range=f'Turnos!A1',
+                            valueInputOption='USER_ENTERED',
+                            body={'values': [new_row_values]})
+                )
+                # Execute the request
+                response = append_request.execute()
+                # Print the updated range and values
+                print(f"Appended range: {response['updates']['updatedRange']}")
+                
+        except HttpError as err:
+            print(err)
 
 class MyLeftPanel:
-    def __init__(self, root, frame, values) -> None:
+    def __init__(self, root, frame) -> None:
         root = root
         frame = frame
 
@@ -55,107 +184,7 @@ class MyLeftPanel:
                     cell = Entry(root, width=10)
                     cell.grid(padx=5, pady=5, row=r+1, column=c)
                     cell.insert(0, '{}'.format(values[r][c]))
-        printall(values)
+        # printall(values)
 
-# actions
-# print all
-def print_all(values):
-    for row in values:
-        print(row)
-
-# print row given its id
-def print_by_id(values, id):
-    for row in values:
-        if row[0] == id:
-            print(row)
-
-# print all rows that match the client name
-def print_filtered_by_client(values, client_name):
-    result = filter(lambda row: client_name.lower() in row[1].lower(), values)
-    print(list(result))
-
-# print all rows that match the model name
-def print_filtered_by_model(values, model_name):
-    result = filter(lambda row: model_name.lower() in row[3].lower(), values)
-    print(list(result))
-
-# print all rows that match the date
-def print_filtered_by_date(values, date):
-    result = filter(lambda row: date in row[5], values)
-    print(list(result))
-
-# print most recent 10 rows
-def print_filtered_by_date_last_ten(values):
-    values.sort(key = lambda row: row[5], reverse=True)
-    print(list(values[0:10]))
-
-# find the row index ("range") given an id
-def find_row_index(sheet_values, column_index, target_value):
-    for i, row in enumerate(sheet_values):
-        if len(row) > column_index and row[column_index] == str(target_value):
-            return i + 1  # Sheets API uses 1-based indices
-    return None
-
-# update row chosen by its id
-def update_by_id(sheet_values, target_id):
-
-    column_name = 'id'
-    new_value = 'New Value'
-
-    # Find the row index based on the 'id' column
-    column_index = sheet_values[0].index(column_name) if sheet_values and column_name in sheet_values[0] else None
-    row_index = find_row_index(sheet_values, column_index, target_id)
-
-    if row_index is not None:
-
-        try:
-            service = build("sheets", "v4", credentials=creds)
-
-            # Call the Sheets API
-            sheet = service.spreadsheets()
-            result = (
-                sheet.values()
-                .update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                        range=f'Turnos!G{row_index}',
-                        valueInputOption='RAW',
-                        body={'values': [[new_value]]})
-            )
-            # Execute the request
-            response = result.execute()
-
-            # Print the updated range and values
-            print(f"Updated range: {response['updatedRange']}")
-            
-        except HttpError as err:
-            print(err)
-
-    else:
-        print(f"Row with '{column_name}' = '{target_id}' not found.")
-
-# add new row to the table
-def add(sheet_values):
-
-    index_to_extract = 0
-    ids_array = [int(sub_array[index_to_extract]) for sub_array in sheet_values] # get array of ids
-    new_row_values = [max(ids_array) + 1, 'Ana', 'agonzalez@gmail.com', 'Samsung S23', 'Boton roto', '2024-02-01', 'asd']
-
-    try:
-            service = build("sheets", "v4", credentials=creds)
-
-            # Call the Sheets API
-            sheet = service.spreadsheets()
-            append_request = (
-                sheet.values()
-                .append(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                        range=f'Turnos!A1',
-                        valueInputOption='USER_ENTERED',
-                        body={'values': [new_row_values]})
-            )
-            # Execute the request
-            response = append_request.execute()
-
-            # Print the updated range and values
-            print(f"Appended range: {response['updates']['updatedRange']}")
-            
-    except HttpError as err:
-        print(err)
+if __name__ == "__main__":
+  main()
--- quickstart.py
+++ quickstart.py
@@ -1,92 +1,27 @@
 # imports
-import os
 from tkinter import *
-# import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-# import dotenv
-from dotenv import load_dotenv
-load_dotenv('./constants.env')
 
 # import functions/methods
 from actions import *
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+  # print all rows
+  # print_all(values)
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = os.environ.get('SPREADSHEET_ID')
-SAMPLE_RANGE_NAME = "Turnos!A2:G20"
+  # print specific row
+  # print_by_id(values, '3')
 
-# initialize data values
-values = [[]]
+  # print_filtered_by_client(values, 'juan')
 
+  # print_filtered_by_model(values, 'iPhone 15')
 
-def main():
-  """Shows basic usage of the Sheets API.
-  Prints values from a sample spreadsheet.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+  # print_filtered_by_date(values, '2024-01-15')
 
-  try:
-    service = build("sheets", "v4", credentials=creds)
+  # print_filtered_by_date_last_ten(values)
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = (
-        sheet.values()
-        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
-        .execute()
-    )
-    global values
-    values = result.get("values", [])
+  # edit specific row
+  # update_by_id(values, 2)
 
-    if not values:
-      print("No data found.")
-      return
-
-    # print all rows
-    # print_all(values)
-
-    # print specific row
-    # print_by_id(values, '3')
-
-    # print_filtered_by_client(values, 'juan')
-
-    # print_filtered_by_model(values, 'iPhone 15')
-
-    # print_filtered_by_date(values, '2024-01-15')
-
-    # print_filtered_by_date_last_ten(values)
-
-    # edit specific row
-    # update_by_id(values, 2)
-
-    # add(values)
-    
-  except HttpError as err:
-    print(err)
+  # add(values)
 
 # interfaz grafica
 root = Tk()
@@ -96,7 +31,7 @@ root.geometry("800x500")
 def btn_nueva_ventana():
   leftFrame = Frame(root)
   leftFrame.grid(row=1, column=0, padx=10, pady=2)
-  leftPanel = MyLeftPanel(root, leftFrame, values)
+  leftPanel = MyLeftPanel(root, leftFrame)
   abrir.destroy()
 
 abrir = Button(root, text="Abrir", command=btn_nueva_ventana)
