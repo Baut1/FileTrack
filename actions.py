f6b793bb669479a10f0610b46c2dfbf969149df8
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-07-30 19:40:32 -0300

    main class structure modified

--- actions.py
+++ actions.py
@@ -70,118 +70,6 @@ def main():
   except HttpError as err:
     print(err)
 
-
-# actions
-# shows grid when given values, called by other filtering and sorting functions
-def show_grid(root, listValues):
-    for r in range(0, len(listValues)):
-            for c in range(0, 7):
-                cell = Entry(root, width=10)
-                cell.grid(padx=5, pady=5, row=r+1, column=c)
-                cell.insert(0, '{}'.format(listValues[r][c]))
-                
-# show all rows
-def show_all(root, values):
-    show_grid(root, values)
-
-# show row given its id
-def show_by_id(root, values, id):
-    for row in values:
-        if row[0] == id:
-            for c in range(0, 7):
-                cell = Entry(root, width=10)
-                cell.grid(padx=5, pady=5, row=1, column=c)
-                cell.insert(0, '{}'.format(row[c]))
-
-# show all rows that match the client name
-def show_filtered_by_client(root, values, client_name):
-    resultFilter = filter(lambda row: client_name.lower() in row[1].lower(), values)
-    resultList = list(resultFilter)
-    show_grid(root, resultList)
-
-# show all rows that match the model name
-def show_filtered_by_model(root, values, model_name):
-    resultFilter = filter(lambda row: model_name.lower() in row[2].lower(), values)
-    resultList = list(resultFilter)
-    show_grid(root, resultList)
-
-# print all rows that match the date
-def print_filtered_by_date(values, date):
-    result = filter(lambda row: date in row[5], values)
-    print(list(result))
-
-# print most recent 10 rows
-def print_filtered_by_date_last_ten(values):
-    values.sort(key = lambda row: row[5], reverse=True)
-    print(list(values[0:10]))
-
-# find the row index ("range") given an id
-def find_row_index(sheet_values, column_index, target_value):
-    try: 
-        for i, row in enumerate(sheet_values):
-            print(row[column_index])
-            if len(row) > column_index and row[column_index] == str(target_value):
-                return i + 2  # Sheets API uses 1-based indices
-        return None
-    except TypeError as err:
-        print(err)
-
-# update row chosen by its id
-def update_by_id(sheet_values, target_id):
-    column_name = 'id'
-    new_value = 'New Value'
-    # Find the row index based on the 'id' column
-    # column_index = sheet_values[0].index(column_name) if sheet_values and column_name in sheet_values[0] else None
-    column_index = 0
-    row_index = find_row_index(sheet_values, column_index, target_id)
-
-    if row_index is not None:
-        try:
-            service = build("sheets", "v4", credentials=creds)
-            # Call the Sheets API
-            sheet = service.spreadsheets()
-            result = (
-                sheet.values()
-                .update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                        range=f'Turnos!G{row_index}',
-                        valueInputOption='RAW',
-                        body={'values': [[new_value]]})
-            )
-            # Execute the request
-            response = result.execute()
-            # Print the updated range and values
-            print(f"Updated range: {response['updatedRange']}")
-            
-        except HttpError as err:
-            print(err)
-    else:
-        print(f"Row with '{column_name}' = '{target_id}' not found.")
-
-# add new row to the table
-def add(sheet_values):
-    index_to_extract = 0
-    ids_array = [int(sub_array[index_to_extract]) for sub_array in sheet_values] # get array of ids
-    new_row_values = [max(ids_array) + 1, 'Ana', 'agonzalez@gmail.com', 'Samsung S23', 'Boton roto', '2024-02-01', 'asd']
-    
-    try:
-            service = build("sheets", "v4", credentials=creds)
-            # Call the Sheets API
-            sheet = service.spreadsheets()
-            append_request = (
-                sheet.values()
-                .append(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                        range=f'Turnos!A1',
-                        valueInputOption='USER_ENTERED',
-                        body={'values': [new_row_values]})
-            )
-            # Execute the request
-            response = append_request.execute()
-            # Print the updated range and values
-            print(f"Appended range: {response['updates']['updatedRange']}")
-            
-    except HttpError as err:
-        print(err)
-
 class MyMainPanel:
     def __init__(self, root, frame) -> None:
         root = root
@@ -197,26 +85,148 @@ class MyMainPanel:
                              command=lambda:show_by_id(root,
                                                        values,
                                                        searchEntry.get())
-                             ).grid(row=8, column=1)
+                             ).grid(row=8,
+                                    column=1)
         
         btnShowByClientName = Button(root,
                                      text="Buscar por cliente",
                                      command=lambda:show_filtered_by_client(root,
-                                                               values,
-                                                               searchEntry.get())
-                                     ).grid(row=8, column=2)
+                                                                            values,
+                                                                            searchEntry.get())
+                                     ).grid(row=8,
+                                            column=2)
         
         btnShowByModel = Button(root,
                                      text="Buscar por modelo",
                                      command=lambda:show_filtered_by_model(root,
-                                                               values,
-                                                               searchEntry.get())
-                                     ).grid(row=8, column=2)
+                                                                           values,
+                                                                           searchEntry.get())
+                                     ).grid(row=8,
+                                            column=3)
         
         btnShowAll = Button(root,
                             text="Ver todos",
-                            command=lambda:show_all(root, values)
-                            ).grid(row=8, column=8)
+                            command=lambda:show_all(root,
+                                                    values)
+                            ).grid(row=8,
+                                   column=8)
+        
+        btnAdd = Button(root,
+                        text="Crear",
+                        command=lambda:add(values)
+                        ).grid(row=8,
+                               column=9)
+
+        # actions
+        # shows grid when given values, called by other filtering and sorting functions
+        def show_grid(root, listValues):
+            for r in range(0, len(listValues)):
+                    for c in range(0, 7):
+                        cell = Entry(root, width=10)
+                        cell.grid(padx=5, pady=5, row=r+1, column=c)
+                        cell.insert(0, '{}'.format(listValues[r][c]))
+
+        # show all rows
+        def show_all(root, values):
+            show_grid(root, values)
+
+        # show row given its id
+        def show_by_id(root, values, id):
+            for row in values:
+                if row[0] == id:
+                    for c in range(0, 7):
+                        cell = Entry(root, width=10)
+                        cell.grid(padx=5, pady=5, row=1, column=c)
+                        cell.insert(0, '{}'.format(row[c]))
+
+        # show all rows that match the client name
+        def show_filtered_by_client(root, values, client_name):
+            resultFilter = filter(lambda row: client_name.lower() in row[1].lower(), values)
+            resultList = list(resultFilter)
+            show_grid(root, resultList)
+
+        # show all rows that match the model name
+        def show_filtered_by_model(root, values, model_name):
+            resultFilter = filter(lambda row: model_name.lower() in row[2].lower(), values)
+            resultList = list(resultFilter)
+            show_grid(root, resultList)
+
+        # print all rows that match the date
+        def print_filtered_by_date(values, date):
+            result = filter(lambda row: date in row[5], values)
+            print(list(result))
+
+        # print most recent 10 rows
+        def print_filtered_by_date_last_ten(values):
+            values.sort(key = lambda row: row[5], reverse=True)
+            print(list(values[0:10]))
+
+        # find the row index ("range") given an id
+        def find_row_index(sheet_values, column_index, target_value):
+            try: 
+                for i, row in enumerate(sheet_values):
+                    print(row[column_index])
+                    if len(row) > column_index and row[column_index] == str(target_value):
+                        return i + 2  # Sheets API uses 1-based indices
+                return None
+            except TypeError as err:
+                print(err)
+
+        # update row chosen by its id
+        def update_by_id(sheet_values, target_id):
+            column_name = 'id'
+            new_value = 'New Value'
+            # Find the row index based on the 'id' column
+            # column_index = sheet_values[0].index(column_name) if sheet_values and column_name in sheet_values[0] else None
+            column_index = 0
+            row_index = find_row_index(sheet_values, column_index, target_id)
+
+            if row_index is not None:
+                try:
+                    service = build("sheets", "v4", credentials=creds)
+                    # Call the Sheets API
+                    sheet = service.spreadsheets()
+                    result = (
+                        sheet.values()
+                        .update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=f'Turnos!G{row_index}',
+                                valueInputOption='RAW',
+                                body={'values': [[new_value]]})
+                    )
+                    # Execute the request
+                    response = result.execute()
+                    # Print the updated range and values
+                    print(f"Updated range: {response['updatedRange']}")
+                    
+                except HttpError as err:
+                    print(err)
+            else:
+                print(f"Row with '{column_name}' = '{target_id}' not found.")
+
+        # add new row to the table
+        def add(sheet_values):
+            index_to_extract = 0
+            ids_array = [int(sub_array[index_to_extract]) for sub_array in sheet_values] # get array of ids
+            new_row_values = [max(ids_array) + 1, 'Ana', 'Samsung S23', '2024-02-01', 'Boton roto', 'agonzalez@gmail.com', 'asd']
+            
+            try:
+                    service = build("sheets", "v4", credentials=creds)
+                    # Call the Sheets API
+                    sheet = service.spreadsheets()
+                    append_request = (
+                        sheet.values()
+                        .append(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=f'Turnos!A1',
+                                valueInputOption='USER_ENTERED',
+                                body={'values': [new_row_values]})
+                    )
+                    # Execute the request
+                    response = append_request.execute()
+                    # Print the updated range and values
+                    print(f"Appended range: {response['updates']['updatedRange']}")
+                    
+            except HttpError as err:
+                print(err)
         
         Label(root, text="ID").grid(row=0, column=0)
         Label(root, text="Nombre").grid(row=0, column=1)
